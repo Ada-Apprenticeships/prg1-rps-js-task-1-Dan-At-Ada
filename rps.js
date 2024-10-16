af4ba22f64ea2ca2463087c3df32f12a1d270d2b
Dan <daniel@ada.ac.uk>
2024-10-16 09:59:01 +0000

    Draw Condition Works :)

--- rps.js
+++ rps.js
@@ -1,12 +1,19 @@
 function rockPaperScissors(player1, player2) {
+  // "rock", "paper", "scissors"
 
+  if(player1 === player2) 
+  {
+    return "draw"
+  }
   
    
 }
 
 
 
-
+// Testing
+rockPaperScissors("rock", "scissors")
+console.log(rockPaperScissors("rock","scissors"))
 
 
 
